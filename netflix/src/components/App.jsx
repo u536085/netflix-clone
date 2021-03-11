de324ef5f135eeddbb77afeea734734c9569dac8
Liam Meijvogel <536085@student.glu.nl>
2021-03-11 13:38:33 +0100

    React setup

---
+++ netflix/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+
+  return (
+    <React.Fragment>
+      <h2 style={{color: "red"}}>{'Halloooo'}</h2>
+    </React.Fragment>
+  );
+ }
+}
+
+export default App;
